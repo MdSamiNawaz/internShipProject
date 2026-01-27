70265f9709f3c169c40e3e782c9d350fbe7caeed
MdSamiNawaz <saminawaz4546@gmail.com>
2026-01-27 22:17:26 +0530

    save

---
+++ script1.js
@@ -0,0 +1,17 @@
+
+document.addEventListener("DOMContentLoaded" , function(){
+const explore = document.getElementById("explore-buton");
+const explores = document.getElementById("ex");
+
+
+
+explore.addEventListener("click", function(){
+     window.location.href = "customerPage.html";
+
+});  
+
+explores.addEventListener("click", function(){
+     window.location.href = "explore.html";
+
+});  
+});
